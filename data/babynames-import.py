1582e89f343c0cdf033f2fac546bc747439e1e3a
Andrew Pilloud <andrew@pilloud.us>
2015-01-12 20:05:48 -0800

    Add code to import standardization datasets

---
+++ data/babynames-import.py
@@ -0,0 +1,57 @@
+import zipfile
+from cStringIO import StringIO
+import re
+
+YOBFILE = re.compile("yob[12][089]\d\d.txt")
+
+def extract_babynames(cur):
+
+	cur.execute("""
+DROP TABLE IF EXISTS ss_babynames;
+	""")
+	cur.execute("""
+CREATE TABLE ss_babynames
+(
+  name varchar(15) NOT NULL,
+  count int NOT NULL,
+  male int NOT NULL,
+  female int NOT NULL
+);
+	""")
+	cur.execute("""
+CREATE INDEX ss_babynames_names_idx
+ ON ss_babynames
+ (name);
+	""")
+
+	with zipfile.ZipFile("babynames.zip", 'r') as nameszip:
+		table = {}
+		for info in nameszip.infolist():
+			if not YOBFILE.match(info.filename):
+				continue
+			for line in nameszip.read(info).split('\r\n'):
+				if not line:
+					continue
+				line = line.split(',')
+				if line[0] not in table:
+					table[line[0]] = {'M': 0, 'F': 0}
+				table[line[0]][line[1]] += int(line[2])
+		for name in table:
+			male = table[name]['M']
+			female = table[name]['F']
+			total = male + female
+			male = male*100/total
+			female = female*100/total
+			cur.execute("INSERT INTO ss_babynames VALUES (?, ?, ?, ?)", [name.upper(), total, male, female])
+
+
+if __name__ == '__main__':
+	import sqlite3
+
+	conn = sqlite3.connect('data.db')
+	cur = conn.cursor()
+
+	extract_babynames(cur)
+
+	conn.commit()
+	conn.close()
---
+++ data/surnames-import.py
@@ -0,0 +1,53 @@
+import zipfile
+from cStringIO import StringIO
+import re
+
+def extract_surnames(cur):
+
+	cur.execute("""
+DROP TABLE IF EXISTS census_surnames;
+	""")
+	cur.execute("""
+CREATE TABLE census_surnames
+(
+  name varchar(15) NOT NULL,
+  count int NOT NULL,
+  white int NOT NULL,
+  black int NOT NULL,
+  api int NOT NULL,
+  aian int NOT NULL,
+  multi int NOT NULL,
+  hispanic int NOT NULL
+);
+	""")
+	cur.execute("""
+CREATE INDEX census_surnames_names_names_idx
+ ON census_surnames
+ (name);
+	""")
+
+	with zipfile.ZipFile("surnames.zip", 'r') as nameszip:
+		for info in nameszip.infolist():
+			if not info.filename == 'app_c.csv':
+				continue
+			for line in nameszip.read(info).split('\n')[1:]:
+				if not line:
+					continue
+				line = line.split(',')
+				stats = []
+				for stat in line[5:]:
+					if stat == '(S)':
+						stat = 0
+					stats.append(int(float(stat)*100))
+				cur.execute("INSERT INTO census_surnames VALUES (?, ?, ?, ?, ?, ?, ?, ?)", [line[0], line[2]] + stats)
+
+if __name__ == '__main__':
+	import sqlite3
+
+	conn = sqlite3.connect('data.db')
+	cur = conn.cursor()
+
+	extract_surnames(cur)
+
+	conn.commit()
+	conn.close()
