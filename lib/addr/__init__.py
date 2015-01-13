3aaa136078e25f152bf9fa20df592e8d9616c097
Andrew Pilloud <andrew@pilloud.us>
2015-01-12 21:03:50 -0800

    Add normalize function for addresses

--- lib/addr/__init__.py
+++ lib/addr/__init__.py
@@ -1,1 +1,21 @@
-from cass import Address
+import cass
+import hashlib
+
+def Normalize(mail, city='', statecode='', zipcode='', cursor=None):
+	a = cass.Address(mail, city, statecode, zipcode)
+	out = a.normalize(cursor)
+
+	if not out:
+		return None
+
+	key = []
+	if 'mail' in out:
+		key.append(out['mail'])
+	if 'city' in out:
+		key.append(out['city'])
+	if 'statecode' in out:
+		key.append(out['statecode'])
+	keystring = ' '.join(key)
+	keyhash = hashlib.sha1(keystring).hexdigest()
+	out['key'] = keyhash
+	return out
